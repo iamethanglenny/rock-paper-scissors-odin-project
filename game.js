784237350391a0fd7d3b39ddf40e73a0ede4214a
iamethanglenny <ethanglenny@gmail.com>
2024-10-17 06:50:34 +0200

    Docs: Add bolierplate to index.html with the script link to game.js and the basic function outlines in game.js

---
+++ game.js
@@ -0,0 +1,19 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice() {
+
+}
+
+function getHumanChoice() {
+
+}
+
+function playRound(humanChoice, computerChoice) {
+
+}
+
+function playGame() {
+
+}
