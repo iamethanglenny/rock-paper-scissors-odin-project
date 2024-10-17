54332d63bb81d19631693177de8ef05a7a768281
iamethanglenny <ethanglenny@gmail.com>
2024-10-17 07:34:44 +0200

    Feat: Add function of playing the round

--- game.js
+++ game.js
@@ -24,8 +24,34 @@ function getHumanChoice() {
 console.log(getHumanChoice())
 
 
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+
 function playRound(humanChoice, computerChoice) {
 
+    // log choices for clarity
+    console.log(`Human choice: ${humanChoice}`);
+    console.log(`Computer choice: ${computerChoice}`);
+
+    // Determine the round winner and update the scores
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!") 
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+        // Human wins
+        console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`)
+        humanScore++
+    } else {
+        // Computer wins
+        console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.`)
+        computerScore++
+    }
+    // Display updated scores
+    console.log(`Human Score: ${humanScore}, Computer Score: ${computerScore}`)
 }
 
 function playGame() {
