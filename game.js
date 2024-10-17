b4aa7001e27262d81243c7137a9ca82b1a50bfa0
iamethanglenny <ethanglenny@gmail.com>
2024-10-17 07:15:23 +0200

    Feat: Add working function for getComputerChoice and getHumanChoice

--- game.js
+++ game.js
@@ -3,12 +3,26 @@ let computerScore = 0;
 
 
 function getComputerChoice() {
-
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return "scissors";
+    } else if (randomNumber === 1) {
+        return "rock";
+    } else {
+        return "paper";
+    }
 }
+console.log(getComputerChoice());
 
 function getHumanChoice() {
-
+    let choice = prompt("Rock, paper, or scissors?").toLowerCase();
+    while (choice !== "rock" && choice !== "scissors" && choice !== "paper") {
+        choice = prompt("Invalid choice. Please enter rock, paper, or scissors:").toLowerCase();
+    } 
+    return choice;
 }
+console.log(getHumanChoice())
+
 
 function playRound(humanChoice, computerChoice) {
 
